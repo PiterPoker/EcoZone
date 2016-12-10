f8fb2b4ae2a3c973a65f1c4a5d348fb3385a1c9b
Pavel <Pavel.Romanov556@yandex.ru>
2016-12-10 17:24:32 +0300

    update model

---
+++ src/EcoZone/Migrations/20161210134308_ApprovedUnits.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ecozone.Migrations
+{
+    public partial class ApprovedUnits : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                "IsApproved",
+                "Units",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                "IsApproved",
+                "Units");
+        }
+    }
+}
