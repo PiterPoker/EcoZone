c4b85bed276c10477df27233affec950f03e6902
Pavel <Pavel.Romanov556@yandex.ru>
2016-12-10 17:26:35 +0300

    apply style

--- src/EcoZone/gulpfile.js
+++ src/EcoZone/gulpfile.js
@@ -16,7 +16,7 @@ paths.minJs = paths.webroot + "js/**/*.min.js";
 paths.css = paths.webroot + "css/**/*.css";
 paths.minCss = paths.webroot + "css/**/*.min.css";
 paths.concatJsDest = paths.webroot + "js/site.min.js";
-paths.concatCssDest = paths.webroot + "css/site.min.css";
+paths.concatCssDest = paths.webroot + "css/main.min.css";
 paths.scss = paths.webroot + "scss/**/*.scss";
 paths.scssDest = paths.webroot + "css/";
 
